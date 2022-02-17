68d8ae2641217c60192b20b76322af544b09c32e
barak9 <31530635+barak9@users.noreply.github.com>
2022-02-17 17:51:28 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -99,7 +99,7 @@ const Cart = () => {
                                         {totalPrice} 
                                         </div>
 
-                                        <button type='button' className='checkout' onClick="">Checkout</button>
+                                       <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_Ix4SWXFrlnf4g6" async> </script> </form>
 
                                     </div>
                                 </div>
